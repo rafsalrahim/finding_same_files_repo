994a182a8fa3194e6aafb708433e10f92e0839c6
Rafsal Rahim <developerscetmca@gmail.com>
2018-07-26 06:38:22 +0530

    An alternative and much faster code to retrive all mp3 files and save to an array

--- finding_file.py
+++ finding_file.py
@@ -3,8 +3,13 @@ import os
 #initialising the directory path to the spacific location
 dir_name = os.getcwd()
 
-for root, dirs, files in os.walk(dir_name):
+'''for root, dirs, files in os.walk(dir_name):
     if files:
         for file in files:
             if os.path.splitext(file)[1] == '.mkv':
-                print(file)
+                print(file)'''
+
+#alternative : faster
+files=os.listdir(cwd)
+files_mkv=[i for i in files if i.endswith('.mkv')]
+print (files_mkv)
