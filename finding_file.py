0902ecb144f298878e3ca6d7ff3d90728eb09d4d
Rafsal Rahim <developerscetmca@gmail.com>
2018-07-26 06:15:12 +0530

    location changed from spackific to any folder in which this script run

--- finding_file.py
+++ finding_file.py
@@ -1,5 +1,9 @@
 import os
-for root, dirs, files in os.walk("/home/rafsal/Videos/Songs"):
+
+#initialising the directory path to the spacific location
+dir_name = os.getcwd()
+
+for root, dirs, files in os.walk(dir_name):
     if files:
         for file in files:
             if os.path.splitext(file)[1] == '.mkv':
